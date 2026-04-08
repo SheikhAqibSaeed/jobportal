7a9138ebbaea9a6862beac196bf0e8eb55c4180f
Aqib Saeed <97892934+SheikhAqibSaeed@users.noreply.github.com>
2026-04-08 19:19:50 +0500

    Add home and contact pages with layout integration
    - Created home.blade.php for the homepage with job listings and categories.
    - Added contact.blade.php for the contact page.
    - Implemented app.blade.php layout for consistent styling across pages.
    - Updated web.php routes to link home and contact pages to HomeController.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,4 +8,10 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
+    public function index(){
+        return view('front.home');
+    }
+    public function contact(){
+        return view('front.contact');
+    }
 }
---
+++ resources/views/front/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('front.layout.app')
+
+@section('main')
+ <h1>This is Contact page.</h1>   
+@endsection
